a803246e17fa6199c9235770f4daa63b9ba1eff7
FakhrizalN <11221057@student.itk.ac.id>
2025-04-14 23:43:57 +0800

    Menambah delete, get all user, get id, update user

---
+++ app/Http/Middleware/AdminAccess.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminAccess
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ($request->user() && $request->user()->tipe === 'admin') {
+            return $next($request);
+        }
+
+        return response()->json([
+            'success' => false,
+            'message' => 'Akses ditolak. Hanya admin yang dapat mengakses endpoint ini.'
+        ], 403);
+    }
+}
