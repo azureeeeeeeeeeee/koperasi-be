4026cd86d4eff72034fe2accfaf1e87fe5e2aa62
FakhrizalN <11221057@student.itk.ac.id>
2025-04-16 08:41:42 +0800

    perbaiki api.php

--- routes/api.php
+++ routes/api.php
@@ -35,13 +35,12 @@ Route::prefix('auth')->middleware('api')->group(function () {
 });
 
 // Admin Routes
-Route::prefix('admin')->group(function () {
-    Route::post('/user', [AdminController::class, 'create_pengguna'])->name('admin.create.pengguna')->middleware('auth:sanctum');
-    Route::delete('/user', [AdminController::class, 'delete_pengguna'])->name('admin.delete.pengguna')->middleware('auth:sanctum');
-    // Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
-    // Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout')->middleware('auth:sanctum');
-    // Route::post('/otp', [AuthController::class, 'sendOtp'])->name('auth.sendOtp');
-    // Route::post('/otp/verify', [AuthController::class, 'verifyOtp'])->name('auth.verifyOtp');
+Route::prefix('admin')->middleware(['auth:sanctum', 'admin'])->group(function () {
+    Route::post('/user', [AdminController::class, 'create_pengguna'])->name('admin.create.pengguna');
+    Route::delete('/user/{id}', [AdminController::class, 'delete_pengguna'])->name('admin.delete.pengguna');
+    Route::get('/user', [AdminController::class, 'get_pengguna'])->name('admin.get.pengguna');
+    Route::get('/user/{id}', [AdminController::class, 'get_pengguna_by_id'])->name('admin.get.pengguna.by.id');
+    Route::put('/user/{id}', [AdminController::class, 'update_pengguna'])->name('admin.update.pengguna');
 });
 
 // Category Routes
